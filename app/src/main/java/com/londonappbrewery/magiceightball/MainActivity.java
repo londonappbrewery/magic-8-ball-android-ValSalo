823a8d98bb33f58c64f920010c894990daf0e193
ValSalo <salovaleriia@gmail.com>
2019-05-29 12:05:45 +0300

    Magic Ball by Valeriia Salo

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -2,6 +2,12 @@ package com.londonappbrewery.magiceightball;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button rollButton;
+        rollButton = (Button) findViewById(R.id.askButton);
+
+        final ImageView Question = (ImageView) findViewById(R.id.imageQuestion);
+
+        final int[] ballArray = {R.mipmap.yes,
+                R.mipmap.no,
+                R.mipmap.maybe,
+                R.mipmap.notnow,
+                R.mipmap.doubt,
+                R.mipmap.later,
+                R.mipmap.sure
+        };
+
+        rollButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("MagicBall", "Ze button has been clicked");
+
+                Random randomNumberGenerator = new Random();
+                int number = randomNumberGenerator.nextInt(6);
+
+                Log.d("MagicBall", "The random answer is: " + number);
+
+                Question.setImageResource(ballArray[number]);
+            }
+        });
     }
 }
